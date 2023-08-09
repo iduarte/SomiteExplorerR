c4597ef591cfe3eec6dfd0e164bc3481470a19b2
I Duarte <iduarte.scientist@gmail.com>
2023-08-10 00:20:16 +0100

    Add length plot

--- extra_R/length-vs-develtime.R
+++ extra_R/length-vs-develtime.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(SomiteExploreR)
 library(car)
+library(ggbeeswarm)
 
 somite_lengths <-
   somite_periods |>
@@ -9,13 +10,31 @@ somite_lengths <-
   dplyr::select(DevelTime, somite_length) |>
   tidyr::drop_na()
 
+# Brown–Forsythe test (because `center = median` which is more robust).
+# https://en.wikipedia.org/wiki/Brown%E2%80%93Forsythe_test
+early_vs_late_length <- car::leveneTest(somite_length ~ DevelTime,
+                                        data = somite_lengths,
+                                        center=median)
+
+print(early_vs_late_length)
+
+
+## Data visualization
 somite_lengths |>
   ggplot(aes(x = DevelTime, y = somite_length)) +
-  geom_beeswarm(cex = 3) +
-  scale_y_continuous(breaks = seq(0, 260, 20), limits = c(0, 260))
+# geom_beeswarm(cex = 3) +
+# geom_boxplot(notch = TRUE) +
+  geom_violin(aes(fill=DevelTime), colour="grey30", alpha=0.5,
+              draw_quantiles=0.5, show.legend = FALSE) +
+  geom_dotplot(fill="grey30", binaxis = "y", stackdir="center", dotsize=0.5) +
+  scale_y_continuous(breaks = seq(0, 280, 20), limits = c(0, 280)) +
+  labs(title = "Length variance | Brown-Forsythe Test",
+       x="Development time",
+       y="Somite length") +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank())
 
-# Brown–Forsythe test (because `center = median`).
-# https://en.wikipedia.org/wiki/Brown%E2%80%93Forsythe_test
-early_vs_late_length <- car::leveneTest(somite_length ~ DevelTime, data = somite_lengths)
+
+
 
 
