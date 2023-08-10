b6bc76be73b80a270ede6a8e729efd86f209f207
I Duarte <iduarte.scientist@gmail.com>
2023-08-10 21:41:31 +0100

    Add scripts with analyses per somite location

---
+++ extra_R/length-vs-somite_location.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(SomiteExploreR)
+library(car)
+library(ggbeeswarm)
+
+somite_lengths <-
+  somite_periods |>
+  dplyr::ungroup() |>
+  dplyr::mutate(somite_location1 = case_when(
+    somite_id %in% c("01", "02", "03", "04", "05", "06", "07", "08", "09") ~ "Somites 1-9",
+    somite_id %in% c("14", "15", "16") ~ "Somites 14-16",
+    somite_id %in% c("17", "18", "19", "20") ~ "Somites 17-20")) |>
+  dplyr::mutate(somite_location2 = case_when(
+    somite_location1 %in% c("Somites 1-9", "Somites 14-16") ~ "Somites 1-16",
+    somite_location1 == "Somites 17-20" ~ "Somites 17-20")) %>%
+  dplyr::mutate(somite_location1 = as.factor(somite_location1),
+                somite_location2 = as.factor(somite_location2)) %>%
+  dplyr::select(somite_location1, somite_location2, somite_length) |>
+  tidyr::drop_na()
+
+# Brown–Forsythe test (because `center = median` which is more robust).
+# https://en.wikipedia.org/wiki/Brown%E2%80%93Forsythe_test
+somites_location1_length <- car::leveneTest(somite_length ~ somite_location1,
+                                        data = somite_lengths,
+                                        center=median)
+
+print(somites_location1_length)
+
+
+## Data visualization
+somite_lengths |>
+  ggplot(aes(x = somite_location1, y = somite_length)) +
+  geom_violin(aes(fill=somite_location1), colour="grey40", alpha=0.5,
+              draw_quantiles=0.5, show.legend = FALSE) +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.3) +
+  scale_y_continuous(breaks = seq(0, 300, 50), limits = c(0, 300)) +
+  theme(panel.grid.minor = element_blank(), text = element_text(size = 13)) +
+  labs(x="Somite number", y="Somite length (micrometre)") +
+  ## Add the p-value
+  annotate(geom="text", x=2, y=287, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(somites_location1_length[[3]][1], digits = 5))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-9", xend="Somites 17-20", y=275, yend=275) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-9", xend="Somites 1-9", y=260, yend=275) +
+  # add middle vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 14-16", xend="Somites 14-16", y=260, yend=275) +
+  theme(panel.grid.minor = element_blank()) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 17-20", xend="Somites 17-20", y=260, yend=275)
+
+
+###############################################################################
+
+# Variance variability in Periods between somites 1-16 and 17-20
+
+# Brown–Forsythe test
+somites_location2_length <-
+  car::leveneTest(somite_length ~ somite_location2,
+                  data = somite_lengths,
+                  center=median)
+print(somites_location2_length)
+
+## Data visualization
+somite_lengths |>
+  ggplot(aes(x = somite_location2, y = somite_length)) +
+  geom_violin(aes(fill=somite_location2), colour="grey40", alpha=0.5,
+              draw_quantiles=0.5, show.legend = FALSE) +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.3) +
+  scale_y_continuous(breaks = seq(0, 300, 50), limits = c(0, 300)) +
+  theme(panel.grid.minor = element_blank(), text = element_text(size = 13)) +
+  scale_fill_manual (values = brewer.pal(8, "Set2")[c(1,4)]) +
+  labs(x="Somite number", y="Somite length (micrometre)") +
+  ## Add the p-value
+  annotate(geom="text", x=1.5, y=287, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(somites_location2_length[[3]][1], digits = 5))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-16", xend="Somites 17-20", y=275, yend=275) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-16", xend="Somites 1-16", y=260, yend=275) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 17-20", xend="Somites 17-20", y=260, yend=275)
---
+++ extra_R/period-vs-somite_location.R
@@ -0,0 +1,148 @@
+library(tidyverse)
+library(SomiteExploreR)
+library(car)
+library(ggbeeswarm)
+library(RColorBrewer)
+
+# Occipital somites 1-5 | Cervical somites 6-9 | Trunk somites 14-20.
+somite_periods2 <-
+  somite_periods |>
+  dplyr::ungroup() |>
+  dplyr::mutate(somite_location = case_when(
+    somite_id %in% c("01", "02", "03", "04", "05") ~ "Occipital",
+    somite_id %in% c("06", "07", "08", "09") ~ "Cervical",
+    somite_id %in% c("14", "15", "16", "17", "18", "19", "20") ~ "Trunk")) |>
+  dplyr::mutate(somite_location = factor(somite_location,
+                                         levels = c("Occipital",
+                                                    "Cervical",
+                                                    "Trunk"))) |>
+  dplyr::select(somite_location, somite_period) |>
+  tidyr::drop_na()
+
+# Brown–Forsythe test (i.e. the Levene's test with `center = median`).
+# https://en.wikipedia.org/wiki/Brown%E2%80%93Forsythe_test
+
+## Occipital_vs_cervical
+occipital_vs_cervical_period <-
+  somite_periods2 %>%
+  filter(somite_location %in% c("Occipital", "Cervical")) %>%
+  car::leveneTest(somite_period ~ somite_location,
+                                        data = .,
+                                        center=median)
+print(occipital_vs_cervical_period)
+
+## Occipital_vs_trunk
+occipital_vs_trunk_period <-
+  somite_periods2 %>%
+  filter(somite_location %in% c("Occipital", "Trunk")) %>%
+  car::leveneTest(somite_period ~ somite_location,
+                  data = .,
+                  center=median)
+print(occipital_vs_trunk_period)
+
+## Cervical_vs_trunk
+cervical_vs_trunk_period <-
+  somite_periods2 %>%
+  filter(somite_location %in% c("Cervical", "Trunk")) %>%
+  car::leveneTest(somite_period ~ somite_location,
+                  data = .,
+                  center=median)
+print(cervical_vs_trunk_period)
+
+
+## Data visualization | Three groups: Somite anatomical locations
+somite_periods2 |>
+  ggplot(aes(x=somite_location, y=somite_period)) +
+  geom_violin(aes(fill=somite_location), colour="grey40", alpha=0.5,
+              draw_quantiles=0.5, show.legend = FALSE) +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.5) +
+  scale_y_continuous(breaks=seq(0, 150, 25), limits=c(0, 165)) +
+  theme(panel.grid.minor = element_blank(), text = element_text(size = 13)) +
+  labs(x="Somite location", y="Somite period (min)") +
+  ## Add p-value 1
+  annotate(geom="text", x=1.5, y=145, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(occipital_vs_cervical_period[[3]][1], digits = 4))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Occipital", xend="Cervical", y=140, yend=140) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Occipital", xend="Occipital", y=135, yend=140) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Cervical", xend="Cervical", y=135, yend=140) +
+
+  ## Add p-value 2
+  annotate(geom="text", x=2.5, y=155, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(occipital_vs_trunk_period[[3]][1], digits = 4))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Cervical", xend="Trunk", y=150, yend=150) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Cervical", xend="Cervical", y=145, yend=150) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Trunk", xend="Trunk", y=145, yend=150) +
+
+## Add p-value 3
+annotate(geom="text", x=2, y=165, color = "grey20", size = 4,
+         label=paste0("P-value = ",
+                      round(cervical_vs_trunk_period[[3]][1], digits = 4))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Occipital", xend="Trunk", y=160, yend=160) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Occipital", xend="Occipital", y=155, yend=160) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Trunk", xend="Trunk", y=155, yend=160)
+
+###############################################################################
+
+# Variance variability in Periods between somites 1-7 and 8-20
+somite_periods3 <-
+  somite_periods |>
+  dplyr::ungroup() |>
+  dplyr::mutate(somite_location = case_when(
+    somite_id %in% c("01","02","03","04","05","06","07") ~ "Somites 1-7",
+    somite_id %in% c("08","09","10","11","12","13","14","15","16","17","18","19","20") ~ "Somites 8-20")) |>
+  dplyr::mutate(somite_location = as.factor(somite_location)) |>
+  dplyr::select(somite_location, somite_period) |>
+  tidyr::drop_na()
+
+# Brown–Forsythe test
+## Somites 1-7 vs 8-20
+somites_1_7_vs_8_20_period <-
+  car::leveneTest(somite_period ~ somite_location,
+                  data = somite_periods3,
+                  center=median)
+print(somites_1_7_vs_8_20_period)
+
+
+## Data visualization | Periods | Somites 1-7 vs 8-20
+somite_periods3 |>
+  ggplot(aes(x=somite_location, y=somite_period)) +
+  geom_violin(aes(fill=somite_location), colour="grey40", alpha=0.5,
+              draw_quantiles=0.5, show.legend = FALSE) +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.5) +
+  scale_y_continuous(breaks=seq(0, 150, 25), limits=c(0, 160)) +
+  theme(panel.grid.minor = element_blank(), text = element_text(size = 13)) +
+  scale_fill_manual (values = brewer.pal(8, "Set2")[c(1,4)]) +
+  labs(x="Somite number", y="Somite period (min)", ) +
+  ## Add p-value 1
+  annotate(geom="text", x=1.5, y=145, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(somites_1_7_vs_8_20_period[[3]][1], digits = 5))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-7", xend="Somites 8-20", y=137, yend=137) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 1-7", xend="Somites 1-7", y=130, yend=137) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Somites 8-20", xend="Somites 8-20", y=130, yend=137)
