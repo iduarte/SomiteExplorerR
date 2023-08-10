f28678ef7e0b0d19349d779c490c20e1ae65e9a6
I Duarte <iduarte.scientist@gmail.com>
2023-08-10 02:00:38 +0100

    Brown–Forsythe test customized plots for period and length

--- extra_R/length-vs-develtime.R
+++ extra_R/length-vs-develtime.R
@@ -22,16 +22,25 @@ print(early_vs_late_length)
 ## Data visualization
 somite_lengths |>
   ggplot(aes(x = DevelTime, y = somite_length)) +
-# geom_beeswarm(cex = 3) +
-# geom_boxplot(notch = TRUE) +
-  geom_violin(aes(fill=DevelTime), colour="grey30", alpha=0.5,
+  geom_violin(aes(fill=DevelTime), colour="grey40", alpha=0.5,
               draw_quantiles=0.5, show.legend = FALSE) +
-  geom_dotplot(fill="grey30", binaxis = "y", stackdir="center", dotsize=0.5) +
-  scale_y_continuous(breaks = seq(0, 280, 20), limits = c(0, 280)) +
-  labs(title = "Length variance | Brown-Forsythe Test",
-       x="Development time",
-       y="Somite length") +
-  theme_minimal() +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.5) +
+  scale_y_continuous(breaks = seq(0, 280, 20), limits = c(0, 290)) +
+  labs(x="Developmental stages", y="Somite length (micrometre)") +
+  scale_fill_manual (values = brewer.pal(8, "Set2")[c(1,4)]) +
+  # add the p-value
+  annotate(geom="text", x=1.5, y=290, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(early_vs_late_length[[3]][1], digits = 5))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Early", xend="Late", y=280, yend=280) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Early", xend="Early", y=270, yend=280) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Late", xend="Late", y=270, yend=280) +
   theme(panel.grid.minor = element_blank())
 
 
--- extra_R/period-vs-develtime.R
+++ extra_R/period-vs-develtime.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(SomiteExploreR)
 library(car)
 library(ggbeeswarm)
+library(RColorBrewer)
 
 somite_periods2 <-
   somite_periods |>
@@ -20,16 +21,25 @@ print(early_vs_late_period)
 ## Data visualization
 somite_periods2 |>
   ggplot(aes(x=DevelTime, y=somite_period)) +
-  # geom_beeswarm(cex=3) +
-  # geom_boxplot(notch=TRUE) +
-  geom_violin(aes(fill=DevelTime), colour="grey30", alpha=0.5,
+  geom_violin(aes(fill=DevelTime), colour="grey40", alpha=0.5,
               draw_quantiles=0.5, show.legend = FALSE) +
-  geom_dotplot(fill="grey30", binaxis = "y", stackdir="center", dotsize=0.5) +
-  scale_y_continuous(breaks=seq(0, 150, 15), limits=c(0, 150)) +
-  labs(title="Period variance | Brown-Forsythe Test",
-       x="Development time",
-       y="Somite period") +
-  theme_minimal() +
+  geom_dotplot(fill="grey40", binaxis = "y", stackdir="center", dotsize=0.5) +
+  scale_y_continuous(breaks=seq(0, 150, 15), limits=c(0, 155)) +
+  labs(x="Developmental stages", y="Somite period (min)") +
+  scale_fill_manual (values=brewer.pal(8, "Set2")[c(1,4)]) +
+  # add p-value
+  annotate(geom="text", x=1.5, y=145, color = "grey20", size = 4,
+           label=paste0("P-value = ",
+                        round(early_vs_late_period[[3]][1], digits = 5))) +
+  # add horizontal line
+  annotate(geom="segment", color = "grey20",
+           x="Early", xend="Late", y=140, yend=140) +
+  # add left vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Early", xend="Early", y=135, yend=140) +
+  # add right vertical tick
+  annotate(geom="segment", color = "grey20",
+           x="Late", xend="Late", y=135, yend=140) +
   theme(panel.grid.minor = element_blank())
 
 
